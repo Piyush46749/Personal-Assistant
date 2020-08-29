f821ae42868367625ca9f93c0023cf6b6b85d635
piyush46749 <piyush46749@gmail.com>
2020-08-29 20:44:03 -0300

    voice properties set

--- assistant.py
+++ assistant.py
@@ -13,3 +13,9 @@ import requests
 from selenium import webdriver
 
 print('Loading your AI personal assistant - Chitti Robot')
+
+voice_id = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Speech\\Voices\\Tokens\\TTS_MS_EN-US_ZIRA_11.0"
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('rate', 190)
+engine.setProperty('voice','voices[1].id')
