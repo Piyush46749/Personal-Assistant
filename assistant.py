070400800965093c5c8a83713a26ca561133cb78
piyush46749 <piyush46749@gmail.com>
2020-09-03 20:09:03 -0300

    weather description

--- assistant.py
+++ assistant.py
@@ -100,3 +100,9 @@ if __name__=='__main__':
             complete_url=base_url+"appid="+api_key+"&q="+city_name
             response = requests.get(complete_url)
             x=response.json()
+            if x["cod"]!="404":
+                y=x["main"]
+                current_temperature = y["temp"]
+                current_humidiy = y["humidity"]
+                z = x["weather"]
+                weather_description = z[0]["description"]
