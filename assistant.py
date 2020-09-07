b3b182388a9af5e3721c9d4520ac775b25beda4e
piyush46749 <piyush46749@gmail.com>
2020-09-07 01:36:46 -0300

    minor changes

--- assistant.py
+++ assistant.py
@@ -155,7 +155,7 @@ if __name__=='__main__':
             time.sleep(4)
 
         elif 'calculate' in statement:
-            speak('I can answer to computational and geographical questions and what question do you want to ask now')
+            speak('I can answer to computational and geographical questions. What question do you want to ask now?')
             question=takeCommand()
             app_id="R2K75H-7ELALHR35X"
             client = wolframalpha.Client('R2K75H-7ELALHR35X')
